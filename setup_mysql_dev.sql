612a27ef816fd21becc8715e9577a19924512a00
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-20 02:16:22 +0300

    task3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privileges on the performance_schema
-GRANT SELECT ON performance.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
