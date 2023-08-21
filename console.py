0dd1becdb0a51ab0306405070615ef44a7d4ec7f
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-22 01:50:10 +0300

    task6

--- console.py
+++ console.py
@@ -247,23 +247,22 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
-    def do_all(self, args):
-        """ Shows all objects, or all objects of a class"""
-        print_list = []
-
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            cls = HBNBCommand.classes[args]
-            for obj in storage.all(cls).values():
-                print_list.append(str(obj))
+    def do_all(self, arg):
+        """Prints string representations of instances"""
+        args = shlex.split(arg)
+        obj_list = []
+        if len(args) == 0:
+            obj_dict = models.storage.all()
+        elif args[0] in classes:
+            obj_dict = models.storage.all(classes[args[0]])
         else:
-            for obj in storage.all().values():
-                print_list.append(str(obj))
-
-        print(print_list)
+            print("** class doesn't exist **")
+            return False
+        for key in obj_dict:
+            obj_list.append(str(obj_dict[key]))
+        print("[", end="")
+        print(", ".join(obj_list), end="")
+        print("]")
 
     def help_all(self):
         """ Help information for the all command """
--- models/state.py
+++ models/state.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" holds class State"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 
-
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """Representation of state """
     from models import storage_type
-    if storage_type == 'db':
+    if storage_type == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
+        cities = relationship("City", backref="state")
     else:
-        name = ''
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if storage_type != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
