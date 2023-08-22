05f933c556f00d32a37054c214f6affcc277f9f3
Fayth7 <flexflakes@gmail.com>
2023-08-22 08:49:54 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -8,7 +8,15 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if storage_type == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
