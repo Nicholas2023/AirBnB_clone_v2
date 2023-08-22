9f20d66e29a8c12a4e4d6d338285a248f46e1088
Fayth7 <flexflakes@gmail.com>
2023-08-22 12:08:26 +0300

    Revert "user.py"
    This reverts commit 05f933c556f00d32a37054c214f6affcc277f9f3.

--- models/user.py
+++ models/user.py
@@ -9,15 +9,7 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if storage_type == "db":
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
