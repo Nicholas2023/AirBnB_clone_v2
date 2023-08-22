225d58ac7a50c7107fe689a3c39607b5fca678a3
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-23 02:03:32 +0300

    task7

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import hashlib
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
