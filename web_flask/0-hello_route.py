38b2e364313b328a92648524da42f4ea2f5890b2
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-23 03:57:15 +0300

    pyco-task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ Start a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Display 'Hello HBNB!' when the root URLis accessed.
     """
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
